8d465335ccc955db37cba4832b60995a0817c40f
Son Hyunbin <147061193+SickofU@users.noreply.github.com>
2025-10-15 16:31:02 +0900

    feat : Initial Setting

---
+++ go.mod
@@ -0,0 +1,15 @@
+module golang-grpc-example
+
+go 1.24.0
+
+require (
+	google.golang.org/grpc v1.76.0
+	google.golang.org/protobuf v1.36.10
+)
+
+require (
+	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+)
---
+++ handler/greeter.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"context"
+	"golang-grpc-example/grpcapi"
+)
+
+type GreeterHandler struct {
+	grpcapi.UnimplementedGreeterServiceServer
+}
+
+func (h *GreeterHandler) SayHello(ctx context.Context, req *grpcapi.SayHelloRequest) (*grpcapi.SayHelloResponse, error) {
+	return &grpcapi.SayHelloResponse{
+		ResponseCode:    200,
+		ResponseMessage: "Hi, Nice to meet you, too",
+	}, nil
+}
